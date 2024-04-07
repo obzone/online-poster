7f862529133f0749046cca4c5670f7a6b2c36060
shaoyecheng <>
2024-04-07 14:31:40 -0400

    fix: carousel position

--- src/components/activity-week-item/activity-week-item.tsx
+++ src/components/activity-week-item/activity-week-item.tsx
@@ -5,27 +5,25 @@ import styles from './activity-week-item.module.scss';
 
 export default async function ActivityWeekItem(props: { data?: Array<Activity>, startDate: Date, currentMonth: Date }) {
   return (
-    <div>
-      <div className={styles.week}>
-        {
-          new Array(7).fill(0).map((_, index) => {
-            const date = new Date(props.startDate.valueOf())
-            date.setDate(date.getDate() + index)
-            const dayInCurrentMonth = date.getMonth() == props.currentMonth.getMonth()
-            const revertData = props.data?.map(item => item).reverse()
-            const currentDayActivities = revertData?.filter(({startTime, endTime}) => {
-              const startTimeObj = new Date(startTime)
-              const endTimeObj = endTime ? new Date(endTime) : undefined
-              return isDateInRange(date, startTimeObj, endTimeObj)
-            })
-            return (
-              <div className={`${styles.day} ${dayInCurrentMonth ? "" : styles.dayNotInCurrentMonth}`} key={index} >
-                <ActivityDayItem activities={currentDayActivities} date={date} />
-              </div>
-            )
+    <div className={styles.week}>
+      {
+        new Array(7).fill(0).map((_, index) => {
+          const date = new Date(props.startDate.valueOf())
+          date.setDate(date.getDate() + index)
+          const dayInCurrentMonth = date.getMonth() == props.currentMonth.getMonth()
+          const revertData = props.data?.map(item => item).reverse()
+          const currentDayActivities = revertData?.filter(({startTime, endTime}) => {
+            const startTimeObj = new Date(startTime)
+            const endTimeObj = endTime ? new Date(endTime) : undefined
+            return isDateInRange(date, startTimeObj, endTimeObj)
           })
-        }
-      </div>
+          return (
+            <div className={`${styles.day} ${dayInCurrentMonth ? "" : styles.dayNotInCurrentMonth}`} key={index} >
+              <ActivityDayItem activities={currentDayActivities} date={date} />
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
