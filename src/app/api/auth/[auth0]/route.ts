f914ff10f034d97c41b9c023debbbcfa5b894282
shaoyecheng <>
2024-04-18 14:27:24 -0400

    feature: login success redirect

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,4 +1,9 @@
 // app/api/auth/[auth0]/route.js
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { env } from 'process';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    returnTo: env.AUTH0_SUCCESS_CALLBACK_URL
+  })
+});
