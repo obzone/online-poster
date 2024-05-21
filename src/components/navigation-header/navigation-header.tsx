d74b2047d191c720e495d7ecfef32b7af8e4c9af
shaoyecheng <>
2024-05-21 13:35:31 -0400

    fix: remove Link optimize

--- src/components/navigation-header/navigation-header.tsx
+++ src/components/navigation-header/navigation-header.tsx
@@ -35,14 +35,14 @@ export default async function NavigationHeader(props: {month: Date, displayBackB
           <Link href={`/organizations`} >
             <div>{organization?.name}</div>
           </Link>
-          <Link href={`/api/ics?month=${props.month.getFullYear()}-${props.month.getUTCMonth()+1}&orgId=${organization?.id}`} >
+          <a href={`/api/ics?month=${props.month.getFullYear()}-${props.month.getUTCMonth()+1}&orgId=${organization?.id}`} >
             <div className="icon-text" >
               <span className="icon has-text-info">
                 <FontAwesomeIcon icon={faCalendarAlt} />
               </span>
               <span>Download</span>
             </div>
-          </Link>
+          </a>
         </div>
       </div>
     </>
